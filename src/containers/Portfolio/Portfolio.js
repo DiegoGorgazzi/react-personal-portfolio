b986115ad5cdd490cf00d562c3bc6595d9a534ce
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-04-30 20:25:43 +0200

    Clean up Portfolio.js for readability

--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -7,91 +7,88 @@ class Portfolio extends Component {
     return (
 
       <div>
-      <nav id="navbar">
-
-          <h4><a href="#welcome-section">About</a></h4>
-          <h4><a href="#projects">Work</a></h4>
-          <h4><a href="#contact">Contact</a></h4>
-
-
-      </nav>
+        <nav id="navbar">
+            <h4><a href="#welcome-section">About</a></h4>
+            <h4><a href="#projects">Work</a></h4>
+            <h4><a href="#contact">Contact</a></h4>
+        </nav>
 
 
-      <section id="welcome-section">
-        <h1>Hi, I'm Mr. Anderson.</h1>
-        <h1>My friends call me Neo.</h1>
-        <h4>I'm a Web Developer</h4>
+        <section id="welcome-section">
+          <h1>Hi, I'm Mr. Anderson.</h1>
+          <h1>My friends call me Neo.</h1>
+          <h4>I'm a Web Developer</h4>
+        </section>
 
-      </section>
+        <section id="projects">
+          <h1>Some of the Projects I did at FCC...</h1>
 
-      <section id="projects">
-        <h1>Some of the Projects I did at FCC...</h1>
+          <div className="projectContainer">
 
-        <div class="projectContainer">
+            <div className="project-title">
+               <picture>
+                 <a href="https://codepen.io/StartHere/full/pxQwGV">
+                   <img
+                     src="https://dl.dropbox.com/s/i28mpp1n8f463cw/Project5Portfolio.png?dl=0"
+                     alt="Project number 5, Personal Portfolio Page"/>
+                  <figcaption>
+                        Project #5: Personal Portfolio
+                  </figcaption>
+                </a>
+              </picture>
+            </div>
 
-          <div class="project-title">
+            <div className="project-title">
              <picture>
-               <a href="https://codepen.io/StartHere/full/pxQwGV">
+               <a href="https://codepen.io/StartHere/full/Edpzbe">
                  <img
-                   src="https://dl.dropbox.com/s/i28mpp1n8f463cw/Project5Portfolio.png?dl=0"
-                   alt="Project number 5, Personal Portfolio Page"/>
+                   src="https://dl.dropbox.com/s/zsoqbc8d4rllcd0/Project4Documentation.png?dl=0"
+                   alt="Project number 4, Documentation Page"/>
+                 <figcaption>
+                   Project #4: Documentation Page
+                 </figcaption> </a>
+            </picture>
+          </div>
+
+            <div className="project-title">
+             <picture>
+              <a href="https://codepen.io/StartHere/full/VErWRL">
+                <img
+                    src="https://dl.dropbox.com/s/ypqmu10qkxni702/Project3ProductPage.png?dl=0"
+                    alt="Project number 3, Product Page"/>
                 <figcaption>
-                      Project #5: Personal Portfolio
+                Project #3: Product Page
                 </figcaption>
               </a>
             </picture>
           </div>
 
-          <div class="project-title">
-           <picture>
-             <a href="https://codepen.io/StartHere/full/Edpzbe">
-               <img
-                 src="https://dl.dropbox.com/s/zsoqbc8d4rllcd0/Project4Documentation.png?dl=0"
-                 alt="Project number 4, Documentation Page"/>
-               <figcaption>
-                 Project #4: Documentation Page
-               </figcaption> </a>
-          </picture>
-        </div>
+             <div className="project-title">
+               <picture>
+                <a href="https://codepen.io/StartHere/full/MPmRNv">
+                  <img src="https://dl.dropbox.com/s/oi7s38d8dy0w2lt/Project2Survey.png?dl=0"
+                       alt="Project number 2, Survey Form"/>
+                  <figcaption>
+                        Project #2: Survey Form
+                  </figcaption>
+                </a>
+              </picture>
+             </div>
 
-          <div class="project-title">
-           <picture>
-        <a href="https://codepen.io/StartHere/full/VErWRL">
-          <img
-            src="https://dl.dropbox.com/s/ypqmu10qkxni702/Project3ProductPage.png?dl=0"
-            alt="Project number 3, Product Page"/>
-        <figcaption>
-              Project #3: Product Page
-          </figcaption> </a>
-       </picture>
-        </div>
+             <div className="project-title">
+               <picture>
+                <a href="https://codepen.io/StartHere/full/qJawxB">
+                  <img
+                    src="https://dl.dropbox.com/s/ak5ppvomibuqfs6/Project1TributePage.png?dl=0"
+                    alt="Project number 1, Tribute Page"/>
+                  <figcaption>
+                    Project #1: Tribute Page
+                  </figcaption> </a>
+               </picture>
+            </div>
 
-           <div class="project-title">
-             <picture>
-       <a href="https://codepen.io/StartHere/full/MPmRNv">
-         <img src="https://dl.dropbox.com/s/oi7s38d8dy0w2lt/Project2Survey.png?dl=0"
-           alt="Project number 2, Survey Form"/>
-        <figcaption>
-              Project #2: Survey Form
-          </figcaption> </a>
-       </picture>
           </div>
-
-           <div class="project-title">
-             <picture>
-       <a href="https://codepen.io/StartHere/full/qJawxB">
-         <img
-           src="https://dl.dropbox.com/s/ak5ppvomibuqfs6/Project1TributePage.png?dl=0"
-           alt="Project number 1, Tribute Page"/>
-        <figcaption>
-              Project #1: Tribute Page
-          </figcaption> </a>
-       </picture>
-          </div>
-
-        </div>
-
-      </section>
+        </section>
 
 
 
@@ -100,39 +97,63 @@ class Portfolio extends Component {
          <h1>Let's work Together</h1>
           <h4>How do you take your coffee?</h4>
 
-        <div class="contactContainer">
+        <div className="contactContainer">
 
-          <div class="contact-title">
-             <div class="circle">
-              <a id="profile-link" href="https://github.com/diegoman1923" target="_blank">Facebook</a>
+          <div className="contact-title">
+             <div className="circle">
+              <a id="profile-link"
+                 href="https://github.com/diegoman1923"
+                 target="_blank"
+                 rel="noopener noreferrer">
+                 Facebook
+              </a>
             </div>
           </div>
 
-          <div class="contact-title">
-             <div class="circle">
-              <a id="profile-link" href="https://github.com/diegoman1923" target="_blank">GitHub</a>
+          <div className="contact-title">
+             <div className="circle">
+              <a id="profile-link"
+                 href="https://github.com/diegoman1923"
+                 target="_blank"
+                 rel="noopener noreferrer">
+                 GitHub
+              </a>
             </div>
           </div>
 
-          <div class="contact-title">
-             <div class="circle">
-              <a id="profile-link" href="https://github.com/diegoman1923" target="_blank">Twitter</a>
+          <div className="contact-title">
+             <div className="circle">
+              <a id="profile-link"
+                 href="https://github.com/diegoman1923"
+                 target="_blank"
+                 rel="noopener noreferrer">
+                 Twitter
+              </a>
             </div>
           </div>
 
-          <div class="contact-title">
-             <div class="circle">
-                <a id="profile-link" href="https://github.com/diegoman1923" target="_blank">Email</a>
+          <div className="contact-title">
+             <div className="circle">
+                <a id="profile-link"
+                    href="https://github.com/diegoman1923"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Email
+                </a>
             </div>
           </div>
 
 
-          <div class="contact-title">
-             <div class="circle">
-              <a id="profile-link" href="https://github.com/diegoman1923" target="_blank">Call me</a>
+          <div className="contact-title">
+             <div className="circle">
+              <a id="profile-link"
+              href="https://github.com/diegoman1923"
+              target="_blank"
+              rel="noopener noreferrer">
+              Call me
+              </a>
             </div>
           </div>
-
 
 
         </div>

80dc35821b2933c1c2c48ca1d6e3f22a3497a2c4
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-02 21:44:11 +0200

    Add Projects component

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Project from "./Project/Project";
+import portfolioStyles from '../../containers/Portfolio/Portfolio.module.css';
+
+const projects = (props) => (
+  <div className={portfolioStyles.projectContainer}>
+    <Project
+      projectURL="https://codepen.io/StartHere/full/pxQwGV"
+      imageSource="https://dl.dropbox.com/s/i28mpp1n8f463cw/Project5Portfolio.png?dl=0"
+      altDescription="Project number 5, Personal Portfolio Page"
+      projectTitle="Project #5: Personal Portfolio"
+    />
+    <Project
+      projectURL="https://codepen.io/StartHere/full/Edpzbe"
+      imageSource="https://dl.dropbox.com/s/zsoqbc8d4rllcd0/Project4Documentation.png?dl=0"
+      altDescription="Project number 4, Documentation Page"
+      projectTitle="Project #4: Documentation Page"
+      />
+
+    <Project
+      projectURL="https://codepen.io/StartHere/full/VErWRL"
+      imageSource="https://dl.dropbox.com/s/ypqmu10qkxni702/Project3ProductPage.png?dl=0"
+      altDescription="Project number 3, Product Page"
+      projectTitle="Project #3: Product Page"
+    />
+
+    <Project
+      projectURL="https://codepen.io/StartHere/full/MPmRNv"
+      imageSource="https://dl.dropbox.com/s/oi7s38d8dy0w2lt/Project2Survey.png?dl=0"
+      altDescription="Project number 2, Survey Form"
+      projectTitle="Project #2: Survey Form"
+    />
+
+    <Project
+      projectURL="https://codepen.io/StartHere/full/qJawxB"
+      imageSource="https://dl.dropbox.com/s/ak5ppvomibuqfs6/Project1TributePage.png?dl=0"
+      altDescription="Project number 1, Tribute Page"
+      projectTitle="Project #1: Tribute Page"
+      />
+</div>
+
+);
+
+export default projects;

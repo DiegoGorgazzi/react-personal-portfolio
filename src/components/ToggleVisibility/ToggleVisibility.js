40954f252e835c111b98babe4fdaa6ef7c3b0637
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-07 14:40:56 +0200

    Remove not used import in ToggleVisibility component

--- src/components/ToggleVisibility/ToggleVisibility.js
+++ src/components/ToggleVisibility/ToggleVisibility.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ToggleVisibilityStyles from './ToggleVisibility.module.css';
-import Projects from "../Projects/Projects";
 
 const toggleVisibility = (props) => {
   if (props.whatState) {

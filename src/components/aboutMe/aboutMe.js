26a7e91dfa266493071cf380453fc88d44465207
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-08 16:26:30 +0200

    Fix css in AboutMe component, update content of AboutMe.js

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -5,9 +5,9 @@ export const aboutMe = (props) => (
 <div className={aboutMeStyles.aboutMe}>
   <h3> I Love Solving Problems </h3>
 
-  <p> I started this journey trying to solve a specific problem and now that
-  problem seems so little with all the other problems I now envision I could help solve
-  just because of my new found knowledge. </p>
+  <p> I started this journey trying to solve a specific problem.  Because of these skills,
+    the funny thing is I now believe that problem is insignificant compared to
+     the other problems I now envision I could help solve. </p>
 
   <p> I did my first website in 2011. It was a WYSIWYG.
   It was limited, terrible, and more importantly, what you
@@ -19,7 +19,9 @@ export const aboutMe = (props) => (
     today. In time, I found the limitations of WordPress as well. </p>
 
   <p> To solve a specific problem I had, I figured I needed to learn
-  Python, which I did a few years ago. This is where I realized
+  Python, which I did a few years ago. </p>
+
+  <p>This is where I realized
   that learning a specific coding language is like learning
   how to make a specific component of a car: you may know how to
   build the engine, but you don't know how to build a car
@@ -33,11 +35,17 @@ export const aboutMe = (props) => (
   and Udemy, I was able to find a clear direction of where I
   needed to go. </p>
 
-  <p> When I graduated from Engineering School almost 20 years ago, I didn't think there was much to
-  learn. I discovered Not only I didn't know anything right out of school, but I learned a new thing
-  every single day of my career (and my humility seemed to have grown exponentially the more I realize how much
-  I don't know and will never know).
-  Old Socrates was right, "I know that I know nothing". The other life lesson I learned is it takes more than
+<p> When I graduated from Engineering School almost 20 years ago, I didn't think
+  there was much more to learn.</p>
+
+<p> I discovered Not only I didn't know anything right out of school,
+  but I learned a new thing  every single day of my career (and my humility
+  seemed to have grown exponentially the more I realized how much
+  I didn't know and will never know).</p>
+
+  <p>You cannot graduate from knowledge.
+  Old Socrates was right, "I know that I know nothing". The other life lesson I
+  learned is it takes more than
   one person to build anything. </p>
 
   <p> So,

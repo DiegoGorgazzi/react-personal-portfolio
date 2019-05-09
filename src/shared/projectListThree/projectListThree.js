0ff667e4e5fff5d82e2c483554975fdc1dad9a4b
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-09 10:48:24 +0200

    Add images for ProjectListThree and update js file

--- src/shared/projectListThree/projectListThree.js
+++ src/shared/projectListThree/projectListThree.js
@@ -1,27 +1,33 @@
+import cashRegister from '../../assets/images/cash-register.png';
+import telephoneNumberValidator from '../../assets/images/telephone-number-validator.png';
+import caesarsCipher from '../../assets/images/caesars-cipher.png';
+import romanNumConverter from '../../assets/images/roman-numeral-converter.png';
+import palindromeChecker from '../../assets/images/palindrome.png';
+
 export const projectListThree = [
 
-  {projectURL:"https://codepen.io/StartHere/full/pxQwGV",
-  imageSource:"https://dl.dropbox.com/s/i28mpp1n8f463cw/Project5Portfolio.png?dl=0",
-  altDescription:"Project number 5, Personal Portfolio Page",
-  projectTitle:"Project #15446515: Personal Portfolio"},
+  {projectURL:"https://codepen.io/StartHere/pen/BejKoL?editors=0011",
+  imageSource: cashRegister,
+  altDescription:"FCC Project number 5, Cash Register",
+  projectTitle:"FCC Project #5: Cash Register"},
 
-  {projectURL:"https://codepen.io/StartHere/full/Edpzbe",
-  imageSource:"https://dl.dropbox.com/s/zsoqbc8d4rllcd0/Project4Documentation.png?dl=0",
-  altDescription:"Project number 4, Documentation Page",
-  projectTitle:"Project #4: Documentation Page"},
+  {projectURL:"https://codepen.io/StartHere/pen/GaooeR?editors=0011",
+  imageSource:telephoneNumberValidator,
+  altDescription:"FCC Project number 4, US Telephone Number Validator",
+  projectTitle:"FCC Project #4: US Telephone Number Validator"},
 
-  {projectURL:"https://codepen.io/StartHere/full/VErWRL",
-  imageSource:"https://dl.dropbox.com/s/ypqmu10qkxni702/Project3ProductPage.png?dl=0",
-  altDescription:"Project number 3, Product Page",
-  projectTitle:"Project #3: Product Page"},
+  {projectURL:"https://codepen.io/StartHere/pen/qGbdxW?editors=0011",
+  imageSource: caesarsCipher,
+  altDescription:"FCC Project number 3, Caesar's Cipher",
+  projectTitle:"FCC Project #3: Caesar's Cipher"},
 
-  {projectURL:"https://codepen.io/StartHere/full/MPmRNv",
-  imageSource:"https://dl.dropbox.com/s/oi7s38d8dy0w2lt/Project2Survey.png?dl=0",
-  altDescription:"Project number 2, Survey Form",
-  projectTitle:"Project #2: Survey Form"},
+  {projectURL:"https://codepen.io/StartHere/pen/pmgJyp?editors=0011",
+  imageSource: romanNumConverter,
+  altDescription:"FCC Project number 2, Roman Numeral Converter",
+  projectTitle:"FCC Project #2: Roman Numeral Converter"},
 
-  {projectURL:"https://codepen.io/StartHere/full/qJawxB",
-  imageSource:"https://dl.dropbox.com/s/ak5ppvomibuqfs6/Project1TributePage.png?dl=0",
-  altDescription:"Project number 1, Tribute Page",
-  projectTitle:"Project #1: Tribute Page"}
+  {projectURL:"https://codepen.io/StartHere/pen/RmWeJK?editors=0011",
+  imageSource: palindromeChecker,
+  altDescription:"FCC Project number 1, Palindrome Checker",
+  projectTitle:"FCC Project #1: Palindrome Checker"}
 ];

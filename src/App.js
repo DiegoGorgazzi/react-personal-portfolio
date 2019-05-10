afd95e5c258b0dc49f08a276c17cfe234eaba69b
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2019-05-10 16:10:19 +0200

    Comment out logo import.. will be needed in the future

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import appStyles from './App.module.css';
 import Portfolio from './containers/Portfolio/Portfolio';
 
